56d08d0f14dabced945ed0a0e1988d5cc4dc6a17
efarinre <72398134+efarinre@users.noreply.github.com>
2021-08-23 22:13:29 +0100

    Add files via upload

---
+++ ucl_dsh_exports/dsh_modelling_ldn.R
@@ -0,0 +1,126 @@
+# read in necessary packages
+library(tidyverse)
+library(INLA)
+library(spdep)
+library(SpatialEpi)
+library(ggplot2)
+library(plyr)
+library(dplyr)
+library(readr)
+library(sf)
+library(rgdal)
+library(tmap)
+library(RSQLite)
+library(tmaptools)
+library(leaflet)
+library(htmltools)
+library(broom)
+library(plotly) 
+library(geojsonio) 
+library(mapview) 
+library(crosstalk)
+library(viridis)
+library(reshape2)
+library(shinyjs)
+library(janitor)
+library(car)
+library(corrplot)
+library(shades)
+library(ggpubr)
+library(rstan)
+library(RDS)
+
+# Modelling Script
+
+# First we need to create a neighbourhood matrix to define the spatial random effect
+#by using the poly2nb() and the nb2INLA() functions of the spdep package (Bivand, 2019).
+nb <- poly2nb(ldn_lsoas)
+summary(nb)
+class(nb)
+
+# create the neighbours matrix
+nb2INLA("london.adj", nb)
+g <- inla.read.graph(filename = "london.adj")
+summary(g)
+head(g)
+
+
+# Now write the formula of the Bernardinelli model:
+
+# Model 0000000000000000000000 zero - just the unstructuctured spatial residual Vi
+# I will then compute MORAN'S I to check for spatial autocorrelation in the RR
+formula0 <- Y ~ log(pop_density) + log(green_access_prop) + log(green_access_dist) +
+  f(area_v, model = "iid")
+
+ldn_model0 <- inla(formula0,
+                   family= "poisson", data = all_vars, E=E,
+                   control.compute = (list(dic = TRUE)),
+                   control.predictor = list(compute = TRUE)
+)
+summary(ldn_model0)
+saveRDS(ldn_model0, "ldn_model0.rds")
+
+
+# NOW for Moran's I
+# We'll keep with the Queen's neighbourhood structure as it is the default (used here) 
+# for the INLA models. We'll also use the Binary spatial weights
+
+# Creating the binary spatial weights matric from the queens neighbourhood structure
+lsoa_weights_m <- nb %>%
+  nb2mat(., style = "B")
+sum(lsoa_weights_m)
+
+# changing the spatial weights matrix into a list as Moran's I requires it this way
+lsoa_weights_m <- nb %>%
+  nb2listw(., style = "B")
+
+# Extract the residuals
+head(ldn_model0$summary.random$area_v)
+residuals_vi <- ldn_model0$summary.random$area_v[,"mean"]
+head(residuals_vi)
+
+
+# Going to use the Monte-Carlo simulation of Moran's I, rather than the basic analytical approach of 
+# The analytical apprach is fast, but the MC simulative approach is a safer approach as it takes an 
+# extra argument n, the number of simulations
+I_Llsoa_binary <- moran.mc(x = residuals_vi, listw = lsoa_weights_m, nsim = 1000)   
+I_Llsoa_binary
+
+
+###### Stil with logged covariates
+
+# 1st only with the green access and popden covariates
+formula1_log <- Y ~ log(pop_density) + log(green_access_prop) + log(green_access_dist) + 
+  f(area_u, model = "besag", graph = g) +
+  f(area_v, model = "iid")
+
+ldn_model1_log <- inla(formula1_log,
+                    family = "poisson", data = all_vars, E = E,
+                    control.compute = (list(dic = TRUE)),
+                    control.predictor = list(compute = TRUE)
+)
+summary(ldn_model1_log)
+#saveRDS(ldn_model1_log, "ldn_model1_log.rds")
+
+
+
+######### 1.5
+# this model initially gave an error as 4 logged values became Inf's, suggesting that there were zeros or negative values
+# After inspection it turns out there are neither negative values nor zeros in any of the columns,
+# however, the social renting column has a few values that are close to zero (e.g. 0.4) 
+# so I will apply  log(social renting pc + 1) and see if it works
+
+# MODEL 1.5 with the green access and popden covariates AND sociodemographic variables. I.e., without
+# the health access variables
+formula1.5_log <- Y ~ log(pop_density) + log(green_access_prop) + log(green_access_dist) + 
+  log(unemployed_pc + 1) + log(socialrenting_pc + 1) + log(bame_pc) +
+  f(area_u, model = "besag", graph = g) +
+  f(area_v, model = "iid")
+
+ldn_model1.5_log <- inla(formula1.5_log,
+                     family = "poisson", data = all_vars, E = E,
+                     control.compute = (list(dic = TRUE)),
+                     control.predictor = list(compute = TRUE)
+)
+summary(ldn_model1.5_log)
+#saveRDS(ldn_model1.5_log, "ldn_model1.5_log.rds")
